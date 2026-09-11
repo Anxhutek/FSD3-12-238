97a559344fadf7a87660e47fbe42193744d65565
Akv Tech <Akvtechmoy@gmail.com>
2026-09-11 15:42:33 +0530

    lab4 done

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,19 +1,20 @@
 import http from 'http';
+import {getusers} from './users.js';
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/api/users', req.method == 'GET') {
-        res.end(JSON.stringify({ msg: "all users"}))
+    if(req.url === '/api/users'&& req.method == 'GET') {
+        res.end(JSON.stringify(getusers()))
     }
-    else if(req.url === '/api/users', req.method == 'POST') {
+    else if(req.url === '/api/users'&& req.method == 'POST') {
         res.end(JSON.stringify({ msg: "add user"}))
     }
-    else if(req.url === 'api/users/1', req.method === 'GET') {
+    else if(req.url === '/api/users/1'&& req.method === 'GET') {
         res.end(JSON.stringify({ msg: "single user with id 1"})) 
     }
-    else if(req.url === 'api/users/1', req.method === 'PUT') {
+    else if(req.url === '/api/users/1'&& req.method === 'PUT') {
         res.end(JSON.stringify({ msg: "update user 1"})) 
     }
-    else if(req.url === 'api/users/1', req.method === 'DELETE') {
+    else if(req.url === '/api/users/1'&& req.method === 'DELETE') {
         res.end(JSON.stringify({ msg: "remove 1"})) 
     } else {
         res.statusCode = 404;
---
+++ lab4/users.js
@@ -0,0 +1,8 @@
+// we use in memory database
+let users=[
+    {id:1,name:'amit sharma',mod:"91xxxxxxx",email:"anxxxxx@gmail.com"},
+    {id:2,name:'monkshara',mod:"561xxxxxxx",email:"sbuwxxxxx@gmail.com"},
+];
+
+let nextId = 3;
+export const getusers = () => users;
