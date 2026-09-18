5584cd3f0533534fee462b7390b2384b83ea5630
Anxhutek <akvtechmoy@gmail.com>
2026-09-18 14:44:55 +0530

    Fix formatting and remove duplicate click handler

--- lab1/dom2.js
+++ lab1/dom2.js
@@ -1,13 +1,9 @@
-import {EventEmitter} from 'events';
+import { EventEmitter } from 'events';
 
-const button =new EventEmitter();
+const button = new EventEmitter();
 
 button.on('click', () => {
     console.log('Button clicked!');
-});
-
-button.on('click', () => {
-    console.log('Another click handler!');
 });
 
 button.emit('click');
